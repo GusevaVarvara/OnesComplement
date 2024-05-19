1c127595682003665181a55cae9b6cbe5701da59
GusevaVarvara <st119025@student.spbu.ru>
2024-05-19 14:01:18 +0300

    Реализовано выполнение операций сложения и вычитания чисел в обратном коде

---
+++ operations.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+std::string toReverseBinary(int num, int num_bits);
+
+int fromReverseBinary(std::string binary);
+
+std::string reverseBinaryAddition(std::string binary1, std::string binary2);
+
+std::string reverseBinarySubtraction(std::string binary1, std::string binary2);
